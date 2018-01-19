bf8ff19c6150aeb6987b12a7fab8f7524f806ec8
1701542-Saimia <anttiyleinen@hotmail.com>
2018-01-19 19:02:22 +0200

    Car-task complete.

---
+++ object-method/car-task/car-task/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using task1;
+
+namespace taskcar
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Car car = new Car( );
+            car.AskData();
+            car.ShowCarInfo();
+            Console.WriteLine();
+            car.Brake();
+
+            Car car1 = new Car();
+            car.AskData();
+            car.ShowCarInfo();
+            Console.WriteLine();
+            
+
+            Car car2 = new Car();
+            car.AskData();
+            car.ShowCarInfo();
+            car.Accelerate();
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ object-method/car-task/car-task/cars.cs
@@ -0,0 +1,104 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task1
+{
+    class Car
+    {
+        //Fields
+        public string Brand;
+        public double Speed;
+
+        //Constructor
+        public Car()
+
+        {
+            Brand = "unknown";
+            Speed = 0;
+        }
+
+        public Car(string brand, int speed)
+        {
+            Brand = brand;
+            this.Speed = speed;
+
+        }
+        public void AskData()
+
+        {
+            //Methods
+            Console.WriteLine($"Syötä auton merkki:");
+            Brand = Console.ReadLine();
+            Console.WriteLine($"Syötä auton nopeus:");
+            Speed = int.Parse(Console.ReadLine());
+        }
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Auton merkki on {Brand}");
+            Console.WriteLine($"Auton nopeus on {Speed}");
+
+        }
+        public void Brake()
+        {
+            Speed = 0.9 * Speed;
+            Console.WriteLine($"alennettu nopeus {Speed}");
+
+        }
+        public void Accelerate()
+        {
+            Console.WriteLine($"Nopeuteen lisätään ");
+
+            if (Speed < 0)
+            {
+                Console.WriteLine("Negatiivinen tulos ei ole mahdollinen");
+
+                
+            }
+            else
+            {
+                Speed++;
+                Console.WriteLine("Uusi nopeus on ");
+            }
+
+            
+        }
+    
+            
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     }
+    }
+
