4b97362b81fa5b25da41fffcb99d4446bb114d3c
1701542-Saimia <anttiyleinen@hotmail.com>
2018-01-19 09:31:24 +0200

    Task-book completed and working.

---
+++ object-method/Class-Book/Class-Book/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace taskbook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            classbook book = new classbook("Harry Potter", "JK ROWLING", 179012, 22.90);
+            book.Printbookinfo();
+            Console.WriteLine();
+
+            classbook nbook = new classbook("Vares", "Ilkka Remes", 170183, 69.90);
+            book.Printbookinfo();
+            Console.WriteLine();
+
+            Console.WriteLine($"{book.CompareBook(nbook)}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ object-method/Class-Book/Class-Book/classbook.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace taskbook
+
+{
+    class classbook
+    {
+    //Fieldsit
+    public string Title;
+    public string Author;
+    public int Id;
+    public double Price;
+    
+    //Constructor ------->
+    public classbook()
+    {
+        Title = "unknown";
+        Author = "unknown";
+        Id = 0;
+        Price = 0;
+    }
+    public classbook(string title, string author, int id, double price)
+    {
+        Title = title;
+        Author = author;
+        this.Id = id;
+        this.Price = price;
+    }
+
+        //Methods
+
+        public void Printbookinfo()
+        {
+            Console.WriteLine($"Kirjan nimi on {Title} ");
+            Console.WriteLine($"Kirjailijan nimi on {Author} ");
+            Console.WriteLine($"Id on {Id} ");
+            Console.WriteLine($"Hinta on {Price} ");
+        }
+            public string CompareBook(classbook book)
+            {
+                if (this.Price > book.Price)
+                {
+                    return $"{this.Title} on kalliimpi kuin {book.Title} kirja";
+                }
+                else
+                {
+                    return $"{this.Title} on halvempi kuin {book.Title} kirja";
+                }
+            }
+        }
+
+
+    }
+
+
+    
+
