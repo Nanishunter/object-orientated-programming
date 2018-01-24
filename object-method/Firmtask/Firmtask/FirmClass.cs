ee1299080afc56cbd4dedbcfa1976f98077bb975
1701542-Saimia <anttiyleinen@hotmail.com>
2018-01-24 10:59:22 +0200

    Pushed

---
+++ object-method/Firmtask/Firmtask/FirmClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firmtask
+{
+    class FirmClass
+    {
+        //Fields
+        public string Title;
+        public string Adress;
+        public int Phone;
+        private double _Outcome;
+        private double _Expense;
+
+        //Constructor 
+
+        public FirmClass()
+        {
+
+            Title = "unknown";
+            Adress = "unknown";
+            Phone = 0;
+            _Outcome = 0;
+            _Expense = 0;
+
+        }
+
+
+        public FirmClass(string title, string adress, int phone, double outcome, double expense)
+        {
+
+            Title = title;
+            Adress = adress;
+            this.Phone = phone;
+            this._Outcome = outcome;
+            this._Expense = expense;
+            //Copy Constructor
+            
+
+        }
+        public FirmClass(FirmClass previousFirmclass)
+            {
+
+        }
+            )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ object-method/Firmtask/Firmtask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Firmtask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
