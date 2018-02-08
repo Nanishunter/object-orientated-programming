9f90e6c0a4ecc218f857bf38857c8ca1f6b1bc55
1701542-Saimia <anttiyleinen@hotmail.com>
2018-02-08 10:31:22 +0200

    Added fields and methods. Price method not working but the code is right. Working on a solution..

---
+++ object-method/Bookauthor/bookauthor/bookauthor/Book.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bookauthor
+{
+    class Book
+    {
+        //Fields
+
+        public string Name;
+        public string Author;
+        public string Publisher;
+        private double _price;
+        static string ThemeName;
+        
+        public Book()
+        {
+            Name = null;
+            Author = null;
+            Publisher = null;
+            _price = 0;
+            ThemeName = null;
+        }
+
+        public Book(string name, string author, string publisher, double price)
+        {
+            Name = name;
+            Author = author;
+            Publisher = publisher;
+            _price = price;
+            
+
+
+
+        }
+        
+        
+
+        public double Price 
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (value > 30 )
+                {
+                    _price = value - (value * 0.1);
+                }
+                else
+                {
+                    _price = value;
+                }
+            }
+
+        }
+        public void GetBookInfo( string name)
+        {
+            if (Name == name)
+            {
+                Console.WriteLine($"Kirjailijan nimi on {Author}, Kirjan nimi on {Name} , Kirjan hinta on {Price} Teeman nimi on {ThemeName} ");
+
+            }
+            else
+            {
+                Console.WriteLine("Kirjaa ei löydy");
+            }
+               
+
+        }
+        public static void ChangeTheme( string theme )
+        {
+            ThemeName = theme;
+            
+
+        }
+        
+        
+
+        
+
+    }
+}
---
+++ object-method/Bookauthor/bookauthor/bookauthor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace bookauthor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Harry Potter", "JkRowling", "Otava", 10 );
+            book.GetBookInfo("Harry Potter");
+            Book.ChangeTheme("sininen");
+            Book book1 = new Book("Harski potter", "JKRowling", "Otava", 15 );
+            book1.GetBookInfo("Harski potter");
+            Book book2 = new Book("Harski potter", "JKRowling", "Otava", 45);
+            
+            book2.GetBookInfo("Harsi potter");
+            Console.ReadKey();
+        }
+    }
+}
