298893a7d68d05e4ff778d167794e2149fb8f446
1701542-Saimia <anttiyleinen@hotmail.com>
2018-04-26 12:28:21 +0300

    Made a test using try-catch.

---
+++ object-method/Try-catchtesting/TryCatchExample/TryCatchExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TryCatchExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku1 = 1, luku2 = 0, osamaara = 0;
+            try
+            {
+                osamaara = luku1 / luku2;
+                Console.WriteLine(osamaara);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Jakolaskua ei voitu suorittaa tuntemattoman virheen takia: " + ex.Message);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
